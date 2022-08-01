670927e727c2d7976609b3cd7ed368e9a0998b41
Abdul Rafay <rafaywork93@gmail.com>
2022-08-01 09:45:51 +0500

    ..

--- routes/auth.js
+++ routes/auth.js
@@ -28,7 +28,7 @@ router.post('/createuser', [
 
             let user = await User.find({ email: req.body.email });
             
-            if (user === []) {
+            if (user) {
                 return res.status(403).json({ error: "User with this email already exist" });
             }
 
